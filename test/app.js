a7fb2d8e1d450b9152d75ceeb7e3ce01e9a20c49
Nikesh B Patel <patelnikeshb@gmail.com>
2022-08-17 22:28:55 +0100

    first commit

---
+++ test/app.js
@@ -0,0 +1,57 @@
+function braintreeAPI(){
+
+}
+
+function getTransactions(){
+
+}
+
+function verifyTransactions(){
+
+}
+
+function getCustomerDetailsFromMerlin(){
+
+}
+
+function writeToMerlin(){
+
+}
+
+function getLogFile(){
+
+}
+
+function loadLogFile(){
+
+}
+
+function retrieveLastEmailFromLog(){
+
+}
+
+function convertTextFileToArray(){
+
+}
+
+function convertObjectToCSV(){
+
+}
+
+function getSqlFromFile(){
+
+}
+
+function getBatchNumberFromMerlin(){
+
+}
+
+function getProductFromJsonConfigFile(){
+
+}
+
+function getFinanceYearFromJsonConfigFile(){
+
+}
+
+console.log("Hello World!");
