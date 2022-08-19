67ec8abdf3f1c92bdca040a0c3dff3ec3897a131
Nikesh B Patel <patelnikeshb@gmail.com>
2022-08-19 19:48:03 +0100

    Created function getTransactionsFromBraintreeBetweenDates and modified function braintreeAPI and it's test as it could not connect to sandbox correctly

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 function braintreeAPI(){
     require( 'dotenv').config(); 
     const braintree = require("braintree");
+    let oBraintreeEnvironment = null;
+
+    if( process.env.ENVIRONMENT == 'sandbox' ){
+        oBraintreeEnvironment = braintree.Environment.Sandbox;
+    }
+    // else{
+    //     oBraintreeEnvironment = braintree.Environment.Production;
+    // }
     
     const oGateway = new braintree.BraintreeGateway( {
-        environment : process.env.ENVIRONMENT,
+        environment : oBraintreeEnvironment,
         merchantId  : process.env.MERCHANTID,
         publicKey   : process.env.PUBLICKEY,
         privateKey  : process.env.PRIVATEKEY
@@ -13,10 +21,28 @@ function braintreeAPI(){
 
 }
 
-function getTransactionsFromBraintreeFromDateCreated( date ){
+function getTransactionsFromBraintreeBetweenDates( startDateTime, endDateTime ){
+    const oGateway = braintreeAPI();
+
+    return new Promise( ( resolve, reject ) => {
+        oGateway.transaction.search( ( search ) => {
+            search.createdAt().between( startDateTime, endDateTime );
+        }, ( err, transactions ) => {
+            console.log('\n Making call to get transactions from braintree.');
+            if ( !transactions ){
+                console.log( 'getTransactionsFromBraintreeBetweenDates: call failed' );
+                reject( err );
+            }
+            else if ( !err ){
+                console.log( 'getTransactionsFromBraintreeBetweenDates: call succeeded ' );
+                resolve( transactions );
+            }
+        } );
+    } );
+
 
 }
 module.exports = {
     braintreeAPI,
-    getTransactionsFromBraintreeFromDateCreated
+    getTransactionsFromBraintreeBetweenDates
 };
--- test/sandboxTest.js
+++ test/sandboxTest.js
@@ -4,20 +4,25 @@ const app = require('../app.js');
 describe( 'braintreeAPI', async () => {
     it( 'should connect to braintree API', async () => {
         const oGateway = app.braintreeAPI();
-        assert.ok( oGateway.config.environment == 'sandbox' );
+        assert.ok( oGateway.config.environment.server == 'api.sandbox.braintreegateway.com' );
     } );
 
 } )
 
-describe ( 'getTransactionsFromBraintreeFromDateCreated', async () => {
+describe ( 'getTransactionsFromBraintreeBetweenDates', async () => {
     const datToday = new Date();
-    const datYesterday = new Date();
+    const datYesterdayMidnight = new Date();
+    
+    datYesterdayMidnight.setDate( datToday.getDate()-1 );
+    datYesterdayMidnight.setHours( 1, 0, 0, 0 );
 
-    datYesterday.setDate( datToday.getDate()-1 );
+    datToday.setHours( 1, 0, 0, 0 );
 
-    it( 'should request for any transactions made yesterday', () => {
-        const oTransactions = app.getTransactionsFromBraintreeFromDateCreated( datYesterday );
 
+    it( 'should request for any transactions made yesterday', async () => {
+        const oResponse = await app.getTransactionsFromBraintreeBetweenDates( datYesterdayMidnight, datToday );
+        assert.ok( oResponse.success == true );
+        console.log ( oResponse.success );
     } );
 
 } );
